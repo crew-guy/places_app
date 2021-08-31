1eea15c2b96c406187abf053f9695db78108eb6e
ankit <69717009+crew-guy@users.noreply.github.com>
2021-08-31 23:53:45 +0530

    change

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  static const routeName = '/add-place';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
