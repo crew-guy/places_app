526009adcd27a6c8d50ca817bd642daf2d642510
ankit <69717009+crew-guy@users.noreply.github.com>
2021-08-31 23:59:37 +0530

    refac

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -10,19 +10,21 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Add a New Place'),
-        ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text('User inputs'),
-              RaisedButton.icon(
-                  elevation: 0,
-                  icon: Icon(Icons.add),
-                  onPressed: () {},
-                  label: Text('Add a place'))
-            ]));
+      appBar: AppBar(
+        title: Text('Add a New Place'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('User inputs'),
+          RaisedButton.icon(
+              elevation: 0,
+              icon: Icon(Icons.add),
+              onPressed: () {},
+              label: Text('Add a place'))
+        ],
+      ),
+    );
   }
 }
